1650fefd6ff333ec6f83345dddf737dbd0a0e42d
Aysun Naghizada <nagizadaysun9@gmail.com>
2025-04-30 23:34:13 +0400

    Rent4U

---
+++ Rent4u/Rent4u/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rent4u.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
